0c949a6f3395164adf0012352196b183fae695c7
prasaisneha <snehaaprasai@gmail.com>
2021-04-17 19:21:20 +0545

    Revert "RESTful API using Python & MySQL"
    This reverts commit e344b5011e26d55ccf5a255d852f92f86e3c394e.

--- pythonProject/config.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flaskext.mysql import MySQL
-
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '0331'
-app.config['MYSQL_DATABASE_DB'] = 'learning'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
